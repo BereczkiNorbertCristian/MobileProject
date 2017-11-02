c8ccbd1e61d3562799364b96ebe5308f0b356f3c
BereczkiNorbertCristian <bnorbertcristian@gmail.com>
2017-11-02 23:25:59 +0200

    Added create book react-side

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,57 +1,199 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, TouchableOpacity,Alert } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+const SendIntentAndroid = require('react-native-send-intent');
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+class Book {
+    title: string;
+    author: string;
+    course: string;
+    email: string;
+    constructor(){}
+    withTitle(title){
+        this.title = title;
+        return this;
+    }
+    withAuthor(author){
+        this.author = author;
+        return this;
+    }
+    withCourse(course){
+        this.course = course;
+        return this;
+    }
+    withEmail(email){
+        this.email = email;
+        return this;
+    }
+    toString(){
+        return "BOOK{"+ this.title + ", " + this.author + ", " + this.email + "}\n";
+    }
+}
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+class BookDetails extends React.Component {
+
+    static navigationOptions ={
+        title: 'Details',
+    };
+
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+
+    }
+
+
+}
+
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'BookUSeller',
+    };
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <View>
+                <Text>Choose wisely:</Text>
+                <Button
+                    onPress={() => navigate('AddBook')}
+                    title="Add new book"
+                />
+                <Button
+                onPress={() => navigate('ListBooks')}
+                title="List current books"
+                />
+            </View>
+        );
+    }
+}
+
+class ListBooksScreen extends React.Component {
+
+    static navigationOptions = {
+        title: 'List of Books',
+    };
+    constructor(props){
+        super(props);
+        this.books = [];
+        this.books.push({bk: new Book().withAuthor('Harry').withTitle('Potter').withCourse('Maths').withEmail('bno@gmail.com')});
+        this.books.push({bk: new Book().withAuthor('John').withTitle('The Old One').withCourse('English').withEmail('peter@gmail.com')});
+        this.books.push({bk: new Book().withAuthor('Norbert').withTitle('El e').withCourse('CS').withEmail('berecn@amazon.com')});
+    }
+
+    goToDetails(book){
+        // navigate('BookDetails',{book: book});
+        // Alert.alert("Ce faci handicapatule?");
+        Alert.alert(book.author);
+    }
+
+    render(){
+        return (
+            <View>
+                {
+                    <FlatList
+                        data={this.books}
+                        renderItem={({item}) => <TouchableOpacity style={styles.listitm} onPress={()=>this.goToDetails(item.bk)}><Text>{item.bk.title}</Text></TouchableOpacity>}
+                    />
+                }
+            </View>
+        );
+    }
+
+}
+
+class AddBookScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Add new book',
+    };
+    constructor(props){
+        super(props);
+        this.state = {title: 'Title', author: 'Author', course: 'Course', email: 'Email'}
+    }
+
+    submit(){
+        SendIntentAndroid.sendMail("bnorbertcristian@gmail.com", "New Book!", this.prepareBody());
+    }
+
+    prepareBody(){
+        let ret = "This are the details for the new book:";
+        ret += "Title: " + this.state.title + "\n";
+        ret += "Author: " + this.state.author + "\n";
+        ret += "Course: " + this.state.course + "\n";
+        ret += "Email: " + this.state.email + "\n";
+        return ret;
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>
+                    Title:
+                </Text>
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(text) => this.setState({title: text})}
+                    value={this.state.text}
+                />
+                <Text>
+                    Author:
+                </Text>
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(text) => this.setState({author: text})}
+                    value={this.state.text}
+                />
+                <Text>
+                    Course:
+                </Text>
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(text) => this.setState({course: text})}
+                    value={this.state.text}
+                />
+                <Text>
+                    Email:
+                </Text>
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(text) => this.setState({email: text})}
+                    value={this.state.text}
+                />
+                <Button
+                    raised
+                    icon={{name: 'check'}}
+                    title='SUBMIT'
+                    onPress={() => this.submit()} />
+
+            </View>
+        );
+    }
+}
+
+const SimpleApp = StackNavigator({
+    Home: { screen: HomeScreen },
+    AddBook: { screen: AddBookScreen },
+    ListBooks: { screen: ListBooksScreen},
 });
 
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+export default class App extends React.Component {
+    render() {
+        return <SimpleApp />;
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    listitm: {
+        padding: 10,
+        marginTop: 3,
+        backgroundColor: '#d9f9b1',
+        alignItems: 'center',
+    }
 });
